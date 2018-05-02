4258c3460a21b0ec71ffacd81f166e9599fd7549
ccrudkin <ccrudkin@gmail.com>
2018-05-02 13:23:11 -0600

    added main script

---
+++ app.js
@@ -0,0 +1,25 @@
+// Special ingredient list
+const liquids = ['water', 'milk', 'vinegar', 'syrup', 'vodka', 'sauce', 
+                'mayonnaise', 'vanilla extract', 'juice', 'oil', 'honey'];
+const drygoods = ['flour', 'salt', 'sugar', 'baking soda', 'baking powder'];
+
+// Base functions
+function transferText(textIn) {
+    document.getElementById('outputRecipe').innerHTML = textIn;
+}
+
+function convertRecipe() {
+    let textIn = document.getElementById('inputRecipe').value;
+    let units = document.getElementById('unitSelector').value;
+    let textOut;
+    if (units === 'toMetric') {
+        textOut = toMetric(textIn);
+    } else if (units === 'toImperial') {
+        textOut = toImperial(textIn);
+    } else {
+        textOut = 'Please choose a conversion';
+    }
+    transferText(textOut);
+}
+
+document.getElementById('convertButton').addEventListener('click', convertRecipe);
